388c9f61121bc6b8b4a10884341fefc5231fa537
ObidjonEshmamatov2000 <77581543+ObidjonEshmamatov2000@users.noreply.github.com>
2022-01-25 10:41:07 +0500

    bot created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import bot.CarFine;
+import lombok.SneakyThrows;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
+    @SneakyThrows
     public static void main(String[] args) {
-        System.out.println("Qalesan, Abbos");
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new CarFine());
+        System.out.println("bot is running... ");
     }
 }
